5a006d899b8dcbcb22b259c971a6a549ef7da5fc
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-06-24 16:55:50 +0900

    Add files via upload

---
+++ 6009.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+c=input()
+print(c)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6010.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n=int(input())
+
+print(n)
+
+
+# In[ ]:
+
+
+
+
