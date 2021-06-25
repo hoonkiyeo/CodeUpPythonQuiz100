3c50383e65ea57b2e760f8132c86b40987fcc4a2
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-06-25 13:44:37 +0900

    Add files via upload

---
+++ 6014.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=input()
+print(a)
+print(a)
+print(a)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6015.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a,b=input().split()
+print(a)
+print(b)
+
+
+# In[ ]:
+
+
+
+
