0f6a08f74397608d69f277e395d318f5f17685f0
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-06-25 13:44:23 +0900

    Add files via upload

---
+++ 6011.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+f = input()
+f = float(f)
+print(f)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6013.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=input() 
+b=input()
+print(b)
+print(a)
+
+
+# In[ ]:
+
+
+
+
