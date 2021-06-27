67494041224249329da7cc8393478449b97ffc5b
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-06-27 11:29:23 +0900

    Add files via upload

---
+++ 6021.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s = input()
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+
+# In[2]:
+
+
+s = input()
+
+for i in range(len(s)):
+    print(s[i])
+
+
+# In[ ]:
+
+
+
+
