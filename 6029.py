98de60d9eb85038ca4a28cc4fb515193c6e1bc65
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-06-29 23:08:46 +0900

    Add files via upload

---
+++ 6029.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=input()
+a=int(a,16) # a = hexadecimal
+print("%o"%a) #octal
+
+
+# In[ ]:
+
+
+
+
---
+++ 6030.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n=input()
+n=ord(n)
+print(n) #ordinal position
+
+
+# In[2]:
+
+
+n = ord(input())
+print(n)
+
+
+# In[ ]:
+
+
+
+
