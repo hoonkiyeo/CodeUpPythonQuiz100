d6651180bdbf41df9eeaa0b64b1db9b26eb365d2
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-06-29 23:08:34 +0900

    Add files via upload

---
+++ 6027.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=input()
+a=int(a)
+print("%x"%a) # %x -> hexadecimal
+
+
+# In[ ]:
+
+
+
+
---
+++ 6028.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=input()
+a=int(a)
+print("%X"%a) #upper hexadecimal
+
+
+# In[ ]:
+
+
+
+
