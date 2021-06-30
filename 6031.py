6a8afaa298b18d328da9fc646c019b0f8481ba9a
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-06-30 21:06:38 +0900

    Add files via upload

---
+++ 6031.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+c=input()
+c=int(c)
+print(chr(c))  # int -> character
+
+
+# In[3]:
+
+
+n = int(input())
+c = chr(n)
+print(c)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6032.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input())
+print(-a)
+
+
+# In[2]:
+
+
+n = int(input())
+n_n = -n
+print(n_n)
+
+
+# In[ ]:
+
+
+
+
