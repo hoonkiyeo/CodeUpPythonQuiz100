476d3ad7b39f72a0fa847632d53859411d2cde39
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-07-01 20:44:32 +0900

    Add files via upload

---
+++ 6038.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n1, n2 = map(int, input().split())
+
+print(n1 ** n2)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6039.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+f1,f2 = map(float, input().split())
+result = f1 ** f2
+print(result)
+
+
+# In[ ]:
+
+
+
+
