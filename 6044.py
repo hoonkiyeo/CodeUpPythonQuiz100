7147c6dbc9500328416d07ddb03fb3b1ff4ee619
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-07-02 19:49:38 +0900

    Add files via upload

---
+++ 6044.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a,b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(round(a/b,2))
+
+
+# In[ ]:
+
+
+
+
