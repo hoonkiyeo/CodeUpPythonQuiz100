7cf75bb0ec445ebf08f0e033359703b2f21f3cc6
Kyle-yeo <69660509+Kyle-yeo@users.noreply.github.com>
2021-07-02 19:48:49 +0900

    Add files via upload

---
+++ 6042.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+f = float(input())
+result = format(f, ".2f")
+print(result)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6043.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+f1,f2 = map(float, input().split())
+result = f1 / f2 
+print(format(result, ".3f"))
+
+
+# In[ ]:
+
+
+
+
