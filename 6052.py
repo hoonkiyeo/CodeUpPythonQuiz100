cdfbb79ceb224f116f11f040f9f41c73f139df70
HealthyBoy95 <69660509+Kyle-yeo@users.noreply.github.com>
2021-07-04 20:26:18 +0900

    Add files via upload

---
+++ 6052.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+n = int(input())
+print(bool(n))
+
+
+# In[8]:
+
+
+n = int(input())
+if n == 0:
+    print(False)
+else:
+    print(True)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6053.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a = bool(int(input()))
+print(not a)
+
+
+# In[3]:
+
+
+a = bool(int(input()))
+
+if a == False:
+    print(True)
+elif a == True:
+    print(False)
+
+
+# In[ ]:
+
+
+
+
