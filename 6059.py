412acb97ca5d1926014771d93b2727f70c61947d
HealthyBoy95 <69660509+Kyle-yeo@users.noreply.github.com>
2021-07-05 10:38:06 +0900

    Add files via upload

---
+++ 6059.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input())
+print(~a) # ~ (bitwise not)
+
+
+# In[ ]:
+
+
+
+
---
+++ 6060.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#sol_1
+a, b = input().split()
+print(int(a) & int(b)) # & (bitwise and)
+
+
+# In[2]:
+
+
+#sol_2
+a,b = map(int, input().split())
+print(a & b)
+
+
+# In[ ]:
+
+
+
+
